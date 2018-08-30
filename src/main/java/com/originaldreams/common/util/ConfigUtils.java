43b44ede03aee39340308394afae370f80051853
yangkaile <guyexing@foxmail.com>
2018-08-30 15:09:59 +0800

    添加RespnoseUtils和ConfigUtils

---
+++ src/main/java/com/originaldreams/common/util/ConfigUtils.java
@@ -0,0 +1,21 @@
+package com.originaldreams.common.util;
+
+/**
+ * 配置类，存储系统用到的一些常量和配置参数
+ */
+public class ConfigUtils {
+    /**
+     * session保存用户ID的key值
+     */
+    public final static String SESSION_USER_ID_KEY = "id";
+
+    /**
+     * Response里返回码的键值
+     */
+    public final static String RESPONSE_RESULT_CODE_KEY = "success";
+
+    /**
+     * Response里表示成功的返回码
+     */
+    public final static int RESPONSE_RESULT_CODE_SUCCESS_VALUE = 0;
+}
